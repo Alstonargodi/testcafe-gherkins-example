bbe327e262d16ab9834cee031a8187e578c58dea
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2024-02-27 11:58:06 +0700

    first commit

--- page-objects/pages/ComputerPage.js
+++ page-objects/pages/ComputerPage.js
@@ -2,7 +2,6 @@ import {Selector, t} from 'testcafe'
 
 class MainPage{
     constructor(){
-
         this.search_btn= Selector('#searchsubmit')
         this.search_box= Selector("input").withAttribute('placeholder','Filter by computer name...')
         this.empty=Selector(".well").withText('Nothing to display')
